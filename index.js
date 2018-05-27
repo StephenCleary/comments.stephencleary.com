4cac9d87b7747fd4f1eb608b8b65dedc8f877819
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-05-26 20:03:12 -0400

    Fix node script.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const fs = require('fs');
 
-for (let post_id in fs.readdirSync('raw')) {
+for (let post_id of fs.readdirSync('raw')) {
     let combined = [];
-    for (let comment in fs.readdirSync(`raw/${post_id}`)) {
-        combined.push(JSON.parse(fs.readFileSync('file', 'utf8')));
+    for (let comment of fs.readdirSync(`raw/${post_id}`)) {
+        combined.push(JSON.parse(fs.readFileSync(`raw/${post_id}/${comment}`, 'utf8')));
     }
     fs.writeFileSync(`docs/${post_id}.json`, JSON.stringify(combined));
 }
