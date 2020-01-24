12a4c749aed64eb090767255b2f15cf18bba1760
Stephen Cleary <github.ourteddybear@xoxy.net>
2020-01-23 19:38:10 -0500

    Use gh-pages.

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 const fs = require('fs');
 
+if (!fs.existsSync('dist'))
+    fs.mkdirSync('dist');
+fs.copyFileSync('CNAME', 'dist/CNAME');
+
 for (let post_id of fs.readdirSync('raw')) {
     let combined = [];
     for (let comment of fs.readdirSync(`raw/${post_id}`)) {
         combined.push(JSON.parse(fs.readFileSync(`raw/${post_id}/${comment}`, 'utf8')));
     }
-    fs.writeFileSync(`docs/${post_id}.json`, JSON.stringify(combined));
+    fs.writeFileSync(`dist/${post_id}.json`, JSON.stringify(combined));
 }
